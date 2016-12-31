b82bbc918250c398c8325f0114468e3f3d2983f7
yangp725 <yangp725@gmail.com>
2016-12-31 21:14:58 +0800

    lc461

---
+++ 461. Hamming Distance.cpp
@@ -0,0 +1,22 @@
+// bitset::count
+#include <iostream>
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        return x^y;
+    }
+
+    // Return the number of onr-bits in the two's complement binary
+    // https://tech.liuchao.me/2016/11/count-bits-of-integer/
+    int bitcount(int n) {
+        int count = 0
+    }
+};
+
+
+
+int main () {
+
+    return 0;
+}
